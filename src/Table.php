926443e2936c25f2f73f35eafaa8178c1025cab5
Sina Bahrami <github@pixbox.co.uk>
2021-07-09 15:12:34 +0200

    Attempt at fixing "missing" clients issue.

--- src/Table.php
+++ src/Table.php
@@ -455,10 +455,13 @@ EOF;
 			return false;
 		}
 
+		$output->setVar("row_count", count($rows));
+
 		# Hack to fix the order (not sure why SQL isn't doing this)
 		if($rows_query['order_by']){
 			str::multidimensionalOrderBy($rows, $rows_query['order_by']);
 		}
+
 
 		if(is_object($row_handler)){
 			//if a custom row handler has been included
