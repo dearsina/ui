98e69b5d1163615c70bba1f36baa5a458adba800
Sina Bahrami <github@pixbox.co.uk>
2021-09-30 17:43:40 +0200

    Introducing the tab prototype placeholder.

---
+++ src/Tab/Prototype.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\UI\Tab;
+
+/**
+ * Not currently in use, but built to be on the same level as Card and Modal.
+ */
+abstract class Prototype extends \App\Common\Prototype {
+
+}
