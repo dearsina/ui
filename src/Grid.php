36e839579e3efe832d77930591f81eaf45467c79
Sina Bahrami <github@pixbox.co.uk>
2022-11-01 13:24:29 +0200

    Temporary bug fix.

--- src/Grid.php
+++ src/Grid.php
@@ -301,7 +301,7 @@ class Grid {
 		}
 
 		foreach($rows['rows'] as $key => $val){
-			if(is_array($val)){
+			if(str::isNumericArray($rows['rows'])){
 				/**
 				 * If the value is itself an array,
 				 * which can be done if there is a
