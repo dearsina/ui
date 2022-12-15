64bf5603abfb880a514a117c4dd6b4a280d43bbe
Sina Bahrami <github@pixbox.co.uk>
2022-12-15 10:06:29 +0200

    Account for cached tables to at times not exist.

--- src/Table.php
+++ src/Table.php
@@ -396,6 +396,11 @@ EOF;
 			$base_query['table'] = $rel_table;
 		}
 
+		# If the table doesn't exist, return false
+		if(!$sql->tableExists($base_query['db'], $base_query['table'])){
+			return false;
+		}
+
 		# UrlDEcode the variables
 		$vars = str::urldecode($vars);
 
