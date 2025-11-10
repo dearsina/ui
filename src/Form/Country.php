deb61a0729ab1fd48fdfa7c9247e455064d62c43
Sina Bahrami <github@pixbox.co.uk>
2025-11-10 14:29:20 +0200

    Allow for country list to be curated.

--- src/Form/Country.php
+++ src/Form/Country.php
@@ -19,9 +19,29 @@ class Country extends Field implements FieldInterface {
 	 */
 	public static function generateHTML(array $a)
 	{
+		$options = \App\Common\Country\Country::getAllCountries(true, $a['language_id']);
+
+		# Include only specific countries
+		if($a['data']['only_countries']){
+			foreach($options as $country_code => $country_name){
+				if(!in_array($country_code, $a['data']['only_countries'])){
+					unset($options[$country_code]);
+				}
+			}
+		}
+
+		# Exclude specific countries
+		if($a['data']['except_countries']){
+			foreach($options as $country_code => $country_name){
+				if(in_array($country_code, $a['data']['except_countries'])){
+					unset($options[$country_code]);
+				}
+			}
+		}
+
 		$a = array_merge($a, [
 			"type" => "select",
-			"options" => \App\Common\Country\Country::getAllCountries(true, $a['language_id']),
+			"options" => $options,
 			"autocomplete" => "country",
 		]);
 
