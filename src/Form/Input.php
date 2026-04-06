614935919cfeb710789f09ba8db3d70d583a2a81
Sina Bahrami <github@pixbox.co.uk>
2026-04-06 16:12:13 +0200

    Add floating label support to form inputs
    Introduced the ability to use floating labels for form inputs, allowing labels to move above the input value when focused or filled. Updated related PHP, CSS, and SCSS files for functionality and styling, and extended FormField configurations to include floating label options.

--- src/Form/Input.php
+++ src/Form/Input.php
@@ -47,9 +47,25 @@ class Input extends Field {
 			$disabled_class = "disabled";
 		}
 
+		# Placeholder
+		$placeholder = str::getAttrTag("placeholder", self::getPlaceholder($placeholder, $name));
+
+		# Floating label
+		if(self::useFloatingLabel($a, $type, $label)){
+			$floating_label = $label;
+			$label = NULL;
+			$default_parent_class = "input-group-floating";
+			$placeholder = " placeholder=\" \"";
+		}
+
+		# Non-floating label
+		else {
+			$default_parent_class = "input-group";
+		}
+
 		# Parent
 		$parent_id = str::getAttrTag("id", $parent_id);
-		$parent_class_array = str::getAttrArray($parent_class, "input-group", $only_parent_class);
+		$parent_class_array = str::getAttrArray($parent_class, $default_parent_class, $only_parent_class);
 		$parent_class = str::getAttrTag("class", $parent_class_array);
 		$parent_style = str::getAttrTag("style", $parent_style);
 
@@ -73,9 +89,6 @@ class Input extends Field {
 		
 		# Style
 		$style = str::getAttrTag("style", $style);
-
-		# Placeholder
-		$placeholder = str::getAttrTag("placeholder", self::getPlaceholder($placeholder, $name));
 
 		# Misc
 		$min = str::getAttrTag("min", $min);
@@ -131,29 +144,30 @@ class Input extends Field {
 		{$icon}
 		{$pre_button}
 		{$pre}
-		<input
-			id="{$id}"
-			type="{$type}"
-			name="{$name}"
-			value="{$value}"
-			{$class}
-			{$placeholder}
-			{$title}
-			{$style}
-			{$min}
-			{$max}
-			{$step}
-			{$autocomplete}
-			{$disabled}
-			{$checked}
-			{$accept}
-			{$capture}
-			{$data}
-		/>
+	<input
+		id="{$id}"
+		type="{$type}"
+		name="{$name}"
+		value="{$value}"
+		{$class}
+		{$placeholder}
+		{$title}
+		{$style}
+		{$min}
+		{$max}
+		{$step}
+		{$autocomplete}
+		{$disabled}
+		{$checked}
+		{$accept}
+		{$capture}
+		{$data}
+	/>
 		{$icon_suffix}
 		{$button}	
 		{$post_button}
 		{$post}
+		{$floating_label}
 	</div>
 	{$desc}
 	{$script}
@@ -181,5 +195,28 @@ EOF;
 			"onChange" => self::getOnChange($a),
 			"onDemand" => $onDemand ?: $ondemand,
 		]));
+	}
+
+	/**
+	 * Determines whether to use a floating label for the given input configuration.
+	 *
+	 * @param array       $a     An array of input attributes and options.
+	 * @param string|null $type  The type of the input element, which may affect the decision to use a floating label.
+	 * @param string|null $label The label associated with the input element, required for a floating label.
+	 *
+	 * @return bool Returns true if a floating label should be used; otherwise, false.
+	 */
+	private static function useFloatingLabel(array $a, ?string $type, ?string $label): bool
+	{
+		if(empty($a['floating_label']) || $label === NULL || trim(strip_tags($label)) === ""){
+			return false;
+		}
+
+		return !in_array($type, [
+			"hidden",
+			"file",
+			"color",
+			"range",
+		], true);
 	}
 }
